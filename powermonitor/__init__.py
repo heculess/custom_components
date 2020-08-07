6547427de5977103608607e83c8c16be489d7971
heculess <heculess@hotmail.com>
2020-08-07 10:10:43 +0800

    powermonitor add grouping attribute

--- powermonitor/__init__.py
+++ powermonitor/__init__.py
@@ -5,7 +5,13 @@ import voluptuous as vol
 
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.discovery import async_load_platform
-from homeassistant.const import CONF_NAME
+
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    SERVICE_TURN_ON,
+    CONF_NAME,
+)
+
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -13,21 +19,34 @@ CONF_GROUP_ID = 'group_id'
 CONF_POWER_KEY = 'power_key'
 CONF_MAX_POWER = 'max_power'
 CONF_MAX_POWER_CONF = 'max_power_config'
+CONF_GROUPS = "groups"
+CONF_AUTO_RESTART = 'auto_restart'
+
+CONF_ID_LIST = 'id_list'
 
 DOMAIN = "powermonitor"
 DATA_POWERMON = DOMAIN
 DEFAULT_POWER_KEY = "load_power"
 DEFAULT_MAX_POWER = 4800
 
+GROUP_CONFIG = vol.Schema(
+    {
+        vol.Required(CONF_GROUP_ID): cv.string,
+        vol.Optional(CONF_POWER_KEY,default=DEFAULT_POWER_KEY): cv.string,
+        vol.Optional(CONF_MAX_POWER ,default=DEFAULT_MAX_POWER): cv.positive_int,
+        vol.Optional(CONF_MAX_POWER_CONF,default=""): cv.string,
+        vol.Optional(CONF_AUTO_RESTART,default=True): cv.boolean,
+    }
+)
+
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
             {
                 vol.Required(CONF_NAME): cv.string,
-                vol.Required(CONF_GROUP_ID): cv.string,
-                vol.Optional(CONF_POWER_KEY,default=DEFAULT_POWER_KEY): cv.string,
-                vol.Optional(CONF_MAX_POWER ,default=DEFAULT_MAX_POWER): cv.positive_int,
-                vol.Optional(CONF_MAX_POWER_CONF,default=""): cv.string,
+                vol.Required(CONF_GROUPS, default={}): vol.All(
+                    cv.ensure_list,vol.All([GROUP_CONFIG]),
+                ),
             }
         )
     },
@@ -35,21 +54,19 @@ CONFIG_SCHEMA = vol.Schema(
 )
 
 
+SERVICE_TURN_ALL_ON = "turn_all_on"
+SERVICE_TURN_ALL_ON_SCHEMA = vol.Schema({vol.Required(CONF_ID_LIST): cv.string})
+
 class PowerMonitor:
     """interface of a power monitor."""
 
-    def __init__(self, group_id, power_key, max_power, max_power_conf, conf_name):
+    def __init__(self, group_id, power_key, max_power, max_power_conf):
         """Init function."""
         self._group_id = group_id
         self._power_key = power_key
         self._max_power = max_power
         self._max_power_conf = max_power_conf
-        self._name = conf_name
-
-    @property
-    def name(self):
-        """Return the name of the PowerMonito."""
-        return self._name
+        self._auto_restart = True
 
     @property
     def group_id(self):
@@ -71,25 +88,65 @@ class PowerMonitor:
         """Return the name of the PowerMonito."""
         return self._max_power_conf
 
+    @property
+    def auto_restart(self):
+        """Return the name of the PowerMonito."""
+        return self._auto_restart
+
+    def set_auto_restart(self, auto_restart):
+        self._auto_restart = auto_restart
+
 
 async def async_setup(hass, config):
     """Set up the asusrouter component."""
 
-    conf = []
+    monitors_conf = []
 
     if DOMAIN in config:
-        conf = config[DOMAIN]
+        monitors_conf = config[DOMAIN][CONF_GROUPS]
 
-        hass.data[DATA_POWERMON] = PowerMonitor(
+    power_monitor = dict()
+    monitors = []
+    for conf in monitors_conf:
+
+        monitor = PowerMonitor(
             conf[CONF_GROUP_ID],
             conf[CONF_POWER_KEY],
             conf[CONF_MAX_POWER],
-            conf[CONF_MAX_POWER_CONF],
-            conf[CONF_NAME]
+            conf[CONF_MAX_POWER_CONF]   
         )
+        
+        monitor.set_auto_restart(conf[CONF_AUTO_RESTART])
+        monitors.append(monitor)
+
+    power_monitor['name'] = config[DOMAIN][CONF_NAME]
+    power_monitor['groups'] = monitors
+    hass.data[DATA_POWERMON] = power_monitor
 
     hass.async_create_task(
         async_load_platform(hass, "sensor", DOMAIN, {}, config)
+    )
+
+
+    async def _turn_all_on(call):
+        """Restart a router."""
+
+        try:
+
+            id_list = call.data[CONF_ID_LIST]
+            if id_list:
+                turn_list = id_list.strip('[]').split(',')
+                for item in turn_list:
+                    item = item.strip(' \'')
+
+                    await hass.services.async_call("switch", SERVICE_TURN_ON, {ATTR_ENTITY_ID: item})
+
+        except Exception as e:
+            _LOGGER.error(e)
+
+            
+    hass.services.async_register(
+        DOMAIN, SERVICE_TURN_ALL_ON, _turn_all_on, schema=SERVICE_TURN_ALL_ON_SCHEMA
     )
     
 
--- powermonitor/sensor.py
+++ powermonitor/sensor.py
@@ -3,8 +3,19 @@ import logging
 import math
 from datetime import datetime
 from homeassistant.helpers.entity import Entity
+from homeassistant.util import dt as dt_util
 from . import PowerMonitor
 from . import DATA_POWERMON
+
+from homeassistant.const import (
+    ATTR_NOW,
+
+    ATTR_ENTITY_ID,
+    SERVICE_TURN_ON,
+
+    EVENT_STATE_CHANGED,
+    EVENT_TIME_CHANGED,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -12,51 +23,39 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the asusrouter."""
 
-    power_monitor = hass.data[DATA_POWERMON]
-
+    monitors = hass.data[DATA_POWERMON]['groups']
+    name = hass.data[DATA_POWERMON]['name']
     devices = []
-    devices.append(PowerMonitorSensor(hass, power_monitor))
+    devices.append(PowerMonitorSensor(hass, name, monitors))
     add_entities(devices, True)
 
-
-class PowerMonitorSensor(Entity):
+class PowerSensor(Entity):
     """Representation of a asusrouter."""
 
-    def __init__(self, hass, monitor_conf):
+    def __init__(self, hass, monitor):
         """Initialize the router."""
-        self._name = monitor_conf.name
-        self._state = None
-        self._group_id = monitor_conf.group_id
-        self._power_key = monitor_conf.power_key
-        self._max_power = monitor_conf.max_power
-        self._max_power_conf = monitor_conf.max_power_conf
+        self._group_id = monitor.group_id
+        self._power_key = monitor.power_key
+        self._max_power = monitor.max_power
+        self._max_power_conf = monitor.max_power_conf
+        self._auto_restart = monitor.auto_restart
         self._last_power_off = ""
         self._ready_to_power_on = ""
         self._hass = hass
+        self._current_power = 0.0
+
+        self._hass.bus.async_listen(EVENT_STATE_CHANGED,self._on_state_change)
+        self._hass.bus.async_listen(EVENT_TIME_CHANGED,self._on_time_change)
+
+        self._devices_power_dict = {}
+        self._state_off_dict = {}
 
     @property
-    def name(self):
-        """Return the name of the ddns."""
-        return self._name
+    def device_to_power_on(self):
+        """Return the device to turn on power."""
+        return self._ready_to_power_on
 
-    @property
-    def state(self):
-        """Return the state of the ddns."""
-        return self._state
-
-    @property  
-    def device_state_attributes(self):
-        """Return the state attributes."""	
-        return {
-            'ready_to_power_on': self._ready_to_power_on,
-        }
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return "w"
-
-    def get_device_power(self,device):
+    def get_device_power(self, device):
         """Fetch device power."""
         try:
           item = self._hass.states.get(device)
@@ -64,6 +63,7 @@ class PowerMonitorSensor(Entity):
             return 0.0
           if item.state != 'on' :
             self._last_power_off = device
+            return 0.0
 
           power = item.attributes.get(self._power_key)
           if not power:
@@ -84,22 +84,146 @@ class PowerMonitorSensor(Entity):
             return self._max_power
           return float(item.state)
 
-    async def async_update(self):
-        """Fetch status from router."""
-        power_count = 0.0
+    def update_current_power(self):
+
+        self._current_power = 0.0
+
+        for device in self._devices_power_dict.keys():
+            power = self.get_device_power(device)
+            if power > 0.0:
+                self._current_power += power
+
+            self._devices_power_dict[device] = power
+
+    async def get_power_count(self):
+ 
         self._last_power_off = ""
         self._ready_to_power_on = ""
 
         try:
-            max_power = self.get_max_power()
-            id_list = self._hass.states.get(self._group_id).attributes.get('entity_id')
-            for device in id_list:
-                power = self.get_device_power(device)
-                if power > 0.0:
-                    power_count += power
-            self._state = "%.2f" % (power_count)
-            if power_count < max_power:
+            if not self._devices_power_dict :
+                self._devices_power_dict = dict.fromkeys(
+                    self._hass.states.get(self._group_id).attributes.get('entity_id'))
+                _LOGGER.debug(" Get group devices : %s", str(self._devices_power_dict))
+
+            self.update_current_power()
+
+            if self._current_power < self.get_max_power():
                 self._ready_to_power_on = self._last_power_off
+
+                if self._auto_restart and self._ready_to_power_on:
+                    _LOGGER.debug(" Turn on the device : %s", self._ready_to_power_on)
+                    await self._hass.services.async_call("switch", 
+                        SERVICE_TURN_ON, {ATTR_ENTITY_ID: self._ready_to_power_on})
+
+            return self._current_power
+                    
+        except  Exception as e:
+            _LOGGER.error(e)
+            return 0.0
+
+
+    async def _on_state_change(self, event):
+        try:
+            state = event.data.get("new_state")
+            if not state:
+                return
+
+            triger_id = event.data.get("entity_id")
+
+            if triger_id == self._group_id and state.state == "off":
+                self._state_off_dict.clear()
+            
+            if triger_id not in self._devices_power_dict.keys():
+                return
+
+            _LOGGER.debug("devices %s states change to %s", triger_id, state.state)
+            if state.state == "off":
+                self._state_off_dict[triger_id] = dt_util.utcnow()
+                return
+
+            if state.state == "on":
+
+                self.update_current_power()
+
+                if triger_id in self._state_off_dict.keys():
+                    del self._state_off_dict[triger_id]
+
+        except Exception as e:
+            _LOGGER.error(e)
+
+    async def _on_time_change(self, event):
+        try:
+            time_now = event.data.get(ATTR_NOW)
+            if not time_now:
+                return
+
+            for device, stamp in self._state_off_dict.items():
+                time_diff = time_now - stamp
+                _LOGGER.debug("state since last change to off")
+                _LOGGER.debug(time_diff)
+                if time_diff.total_seconds() > 10:
+                    if self._current_power < self.get_max_power():
+                        await self._hass.services.async_call("switch", 
+                            SERVICE_TURN_ON, {ATTR_ENTITY_ID: device})
+                        self._state_off_dict[device] = time_now
+                        return
+
+        except Exception as e:
+            _LOGGER.error(e)
+       
+
+class PowerMonitorSensor(Entity):
+    """Representation of a asusrouter."""
+
+    def __init__(self, hass, name, monitors):
+        """Initialize the router."""
+        self._name = name
+        self._state = None
+        self._ready_to_power_on = []
+        self._monitor_list = []
+        self._hass = hass
+
+        for monitor in monitors:
+            self._monitor_list.append(PowerSensor(hass, monitor))   
+
+    @property
+    def name(self):
+        """Return the name of the ddns."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the ddns."""
+        return self._state
+
+    @property  
+    def device_state_attributes(self):
+        """Return the state attributes."""	
+        return {
+            'ready_to_power_on': self._ready_to_power_on if self._ready_to_power_on else ""
+        }
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return "w"
+
+    async def async_update(self):
+        """Fetch status from router."""
+        try:
+            self._ready_to_power_on = []
+
+            power_count = 0.0
+
+            for monitor in self._monitor_list:
+                power_count += await monitor.get_power_count()
+
+                if monitor.device_to_power_on :
+                    self._ready_to_power_on.append(monitor.device_to_power_on)
+
+            self._state = "%.2f" % (power_count)
+        
         except  Exception as e:
             _LOGGER.error(e)
             self._state = "0.0"
